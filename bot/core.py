22584a789e140a2883a24ddb88b445e80dd2725d
Andrii Kulikov <andrii.kulikov@admiralmarkets.com>
2017-05-25 15:43:29 +0300

    Some cleanups

--- bot/core.py
+++ bot/core.py
@@ -2,37 +2,38 @@ import os
 import time
 from slackclient import SlackClient
 
-
-# starterbot's ID as an environment variable
 BOT_ID = os.environ.get("BOT_ID")
 
-# constants
 AT_BOT = "<@" + BOT_ID + ">"
 EXAMPLE_COMMAND = "do"
 
-# instantiate Slack & Twilio clients
 slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
 
 
 def handle_command(command, channel):
 
-    response = "Not sure what you mean. Use the *" + EXAMPLE_COMMAND + \
-               "* command with numbers, delimited by spaces."
+    response = "`Didn't get it...`"
     if command.startswith(EXAMPLE_COMMAND):
         response = "Sure...write some more code then I can do that!"
-    slack_client.api_call("chat.postMessage", channel=channel,
-                          text=response, as_user=True)
+    slack_client.api_call(
+        "chat.postMessage",
+        channel=channel,
+        text=response,
+        as_user=True
+    )
 
 
 def parse_slack_output(slack_rtm_output):
 
     output_list = slack_rtm_output
     if output_list and len(output_list) > 0:
+
         for output in output_list:
             if output and 'text' in output and AT_BOT in output['text']:
-                # return text after the @ mention, whitespace removed
-                return output['text'].split(AT_BOT)[1].strip().lower(), \
-                       output['channel']
+                return (
+                    output['text'].split(AT_BOT)[1].strip().lower(),
+                    output['channel']
+                )
     return None, None
 
 
