a157db4efd8939b02da4add97dc178b1624c5b9f
Andrii Kulikov <andrii.kulikov@admiralmarkets.com>
2017-05-25 21:00:01 +0300

    Added friends(but everything is hardcoded

--- settings.py
+++ settings.py
@@ -3,8 +3,8 @@ OW_COMMAND = "ow"
 # TODO: Move to redis/cache - create init function
 USER_MAPPING = {
     "jade": "Jadeskycore-2824",
-    "moviedogball": "",
-    "hungson.abadeer": "",
+    "moviedogball": "MDB-212344",
+    "hungson.abadeer": "InSoulEmpty-2817",
 }
 
 BASE_OWAPI_URL = "https://owapi.net/api/v3/u/{battletag}/stats"
