f0b17df1b15f62ff44abf66a55b8a593f3be7883
Jadeskycore <jadecoresky@gmail.com>
2017-06-07 22:12:31 +0300

    Added normal task

--- bot/celery.py
+++ bot/celery.py
@@ -11,4 +11,4 @@ app.conf.update(BROKER_URL=os.environ['REDIS_URL'])
 def setup_periodic_tasks(sender, **kwargs):
     from bot.tasks import hello
     # Calls test('hello') every 10 seconds.
-    sender.add_periodic_task(10.0, hello(), name='add every 10')
+    sender.add_periodic_task(30.0, hello(), name='add every 10')
--- bot/tasks.py
+++ bot/tasks.py
@@ -1,5 +1,17 @@
 from bot.celery import app
+from bot.slack import slack_backend
+from bot.utils import import_string
+from plugins.settings import PLUGIN_CLASSES
+
+files_cls_str = PLUGIN_CLASSES['files']
+files_cls = import_string(files_cls_str)
+
 
 @app.task
 def hello():
-    print(1+1)
+    files_plugins = [
+        p for p in slack_backend._plugins if isinstance(p, files_cls)
+    ]
+    file_plugin = files_plugins[0]
+
+    file_plugin.randomize('bot_testing')
