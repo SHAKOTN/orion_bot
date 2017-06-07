b26ac7e574921720dd1dd29b723581afc6f2c282
Jadeskycore <jadecoresky@gmail.com>
2017-06-08 00:09:27 +0300

    Removed cycle imports

--- bot/celery.py
+++ bot/celery.py
@@ -2,8 +2,6 @@ import os
 
 import celery
 
-from bot.tasks import hello
-
 app = celery.Celery('celery')
 
 app.conf.update(BROKER_URL=os.environ['REDIS_URL'])
@@ -11,4 +9,5 @@ app.conf.update(BROKER_URL=os.environ['REDIS_URL'])
 
 @app.on_after_configure.connect
 def add_periodic(**kwargs):
+    from bot.tasks import hello
     app.add_periodic_task(10.0, hello(), name='add every 10')
