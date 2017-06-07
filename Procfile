024f4ff370f90f3f4425a12f496f5b2d7c03261d
Jadeskycore <jadecoresky@gmail.com>
2017-06-08 00:15:07 +0300

    Trying to beat celery

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 orion: python bot/core.py
-worker: celery worker --app=bot.celery.app -B -l debug --purge
+worker: celery worker --app=bot.celery.app -B -l debug
--- bot/celery.py
+++ bot/celery.py
@@ -4,10 +4,10 @@ import celery
 
 app = celery.Celery('celery')
 
-app.conf.update(BROKER_URL=os.environ['REDIS_URL'])
+app.conf.update(broker_url=os.environ['REDIS_URL'])
 
 
 @app.on_after_configure.connect
 def add_periodic(**kwargs):
     from bot.tasks import hello
-    app.add_periodic_task(10.0, hello(), name='add every 10')
+    app.add_periodic_task(10.0, hello.s(), name='add every 10')
