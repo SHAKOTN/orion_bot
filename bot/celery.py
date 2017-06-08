040eaa6eae31323fec103e85dc109ae1cea76504
Jadeskycore <jadecoresky@gmail.com>
2017-06-08 19:15:08 +0300

    Changed celery broker from redis to cloudamqp

--- bot/celery.py
+++ bot/celery.py
@@ -6,6 +6,6 @@ if os.environ.get('CELERY_ENABLED') == '1':
 
     app = celery.Celery('celery')
 
-    app.conf.update(broker_url=os.environ['REDIS_URL'])
+    app.conf.update(broker_url=os.environ['CLOUDAMQP_URL'])
     app.conf.update(redis_max_connections=3)
 
