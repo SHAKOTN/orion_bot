c4b2e34a1272291c11f39ad655b2704cc549a57a
Jadeskycore <jadecoresky@gmail.com>
2017-06-08 20:22:08 +0300

    Added weather task

--- bot/celery.py
+++ bot/celery.py
@@ -12,10 +12,19 @@ if os.environ.get('CELERY_ENABLED') == '1':
 
     @app.on_after_configure.connect
     def add_periodic(**kwargs):
-        from bot.tasks import post_random_webm
+        from bot.tasks import (
+            post_random_webm,
+            post_morning_weather
+        )
         app.add_periodic_task(
             crontab(minute=0,
                     hour='7,8,9,10,11,12,13,14,15,16,17,18'),
             post_random_webm.s(),
             name='Post random WEBM'
+        )
+
+        app.add_periodic_task(
+            crontab(),
+            post_morning_weather.s(),
+            name='Post morning weather'
         )
--- bot/tasks.py
+++ bot/tasks.py
@@ -4,7 +4,9 @@ from bot.utils import import_string
 from plugins.settings import PLUGIN_CLASSES
 
 files_cls_str = PLUGIN_CLASSES['files']
+weather_cls_str = PLUGIN_CLASSES['weather']
 
+cities = ['kiev', 'tallinn']
 
 @app.task
 def post_random_webm():
@@ -16,3 +18,19 @@ def post_random_webm():
     file_plugin = files_plugins[0]
 
     file_plugin.randomize('games')
+
+
+@app.task
+def post_morning_weather():
+    weather_cls = import_string(weather_cls_str)
+    weather_plugins = [
+        p for p in slack_backend.plugins
+        if isinstance(p, weather_cls)
+    ]
+
+    weather_plugin = weather_plugins[0]
+    for city in cities:
+        weather_plugin.send_weather_current(
+            channel='bot_testing',
+            city=city
+        )
