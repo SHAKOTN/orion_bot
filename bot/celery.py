6f1e5c519b5455457bde0711914077ec369ef199
Jadeskycore <jadecoresky@gmail.com>
2017-06-08 11:49:52 +0300

    Changed task names

--- bot/celery.py
+++ bot/celery.py
@@ -13,17 +13,17 @@ app.conf.update(redis_max_connections=3)
 def add_periodic(**kwargs):
     from bot.tasks import post_random_webm
     app.add_periodic_task(
-        crontab(minute='*/15'),
+        crontab(minute='*/50'),
         post_random_webm.s(),
-        name='Post random WEBM'
+        name='Post random webm every 50min'
     )
     app.add_periodic_task(
         crontab(hour=11, minute=0),
         post_random_webm.s(),
-        name='Post random WEBM'
+        name='Post random WEBM 11a.m'
     )
     app.add_periodic_task(
         crontab(hour=13, minute=0),
         post_random_webm.s(),
-        name='Post random WEBM'
+        name='Post random WEBM 13a.m'
     )
